1b8e2446136653f2cb70b8f79543424f1e8e418f
Erika Forslöf <erikaforslov@hotmail.com>
2017-02-12 00:40:56 +0100

    Added 2 messages, lördag and fredag

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ import discord
 import secret
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
+logging.basicConfig(filename='/usr/local/src/sthlmesport-bot/log/sthlmesportbot.log',level=logging.INFO)
 
 client = discord.Client()
 
@@ -14,6 +13,12 @@ async def on_message(message):
  	   return
 	if message.content.startswith('!hello'):
 		msg = 'Hello {0.author.mention}'.format(message)
+		await client.send_message(message.channel, msg)
+	if message.content.startswith('!fredagsmys') or message.content.startswith('!fredag'):
+		msg = 'På Fredagar är det Fredagsmys!'.format(message)
+		await client.send_message(message.channel, msg)
+	if message.content.startswith('!lördag'):
+		msg = 'Snart är det Måndag igen!~'.format(message)
 		await client.send_message(message.channel, msg)
 	if message.content.startswith('ass'):
 		await client.delete_message(message)
