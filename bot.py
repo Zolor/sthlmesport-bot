92dffbbd218b76f3abac0a72531593da333be1df
Erika Forslöf <erikaforslov@hotmail.com>
2017-02-11 01:44:41 +0100

    Fixed basic bot

--- bot.py
+++ bot.py
@@ -1,17 +1,26 @@
 from discord.ext.commands import Bot
 import discord
 import secret
+import logging
 
-sthlmesportbot = Bot(command_prefix="!")
+logging.basicConfig(level=logging.INFO)
+
+#sthlmesportbot = Bot(command_prefix="!")
 client = discord.Client()
 
-@sthlmesportbot.event
-async def on_read():
-	print("Client logged in")
+#@sthlmesportbot.event
+#async def on_read():
+#	print("Client logged in")
 
-@sthlmesportbot.command()
-async def hello(*args):
-	return await sthlmesportbot.say('Hello!')
+#@sthlmesportbot.command()
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+ 	   return
+	if message.content.startswith('!hello'):
+		msg = 'Hello {0.author.mention}'.format(message)
+		await client.send_message(message.channel, msg)
+	#return await sthlmesportbot.say('Hello!' message.author.mention)
 
 @client.event
 async def on_member_join(member):
@@ -19,5 +28,5 @@ async def on_member_join(member):
     fmt = 'Welcome {0.mention} to {1.name}!'
     await client.send_message(server, fmt.format(member, server))
 
-sthlmesportbot.run(secret.BOT_TOKEN)
+client.run(secret.BOT_TOKEN)
 
