9ddacfec12125c7991d8ea3def59a11e3dda0dcf
Erika Forslöf <erika.forslof@king.com>
2017-02-11 18:27:43 +0100

    Started testing profanity filter

--- bot.py
+++ bot.py
@@ -5,14 +5,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-#sthlmesportbot = Bot(command_prefix="!")
+
 client = discord.Client()
 
-#@sthlmesportbot.event
-#async def on_read():
-#	print("Client logged in")
-
-#@sthlmesportbot.command()
 @client.event
 async def on_message(message):
 	if message.author == client.user:
@@ -20,7 +15,8 @@ async def on_message(message):
 	if message.content.startswith('!hello'):
 		msg = 'Hello {0.author.mention}'.format(message)
 		await client.send_message(message.channel, msg)
-	#return await sthlmesportbot.say('Hello!' message.author.mention)
+	if message.content.startswith('ass'):
+		await client.delete_message(message)
 
 @client.event
 async def on_member_join(member):
@@ -29,5 +25,6 @@ async def on_member_join(member):
     nu_pm = 'Welcome to {0.name}! Make sure to read the rules in <#220934558164451328> . Best Regards, STHLM E-sport'
     await client.send_message(server, fmt.format(member, server))
     await client.send_message(member, nu_pm.format(server))
-client.run(secret.BOT_TOKEN)
 
+
+client.run(secret.BOT_TOKEN)
